eb847127cad6b7d66fd34b967ba1e90e416005b8
AndreaDex <desiderio.andrea@outlook.it>
2021-06-21 15:37:40 +0200

    create html base strucuture, add a php variable

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php 
+$sonetto = "Non sia mai ch’io ponga impedimenti
+all’unione di anime fedeli; Amore non è Amore
+se muta quando scopre un mutamento
+o tende a svanire quando l’altro s’allontana.
+Oh no! Amore è un faro sempre fisso
+che sovrasta la tempesta e non vacilla mai;
+è la stella-guida di ogni sperduta barca,
+il cui valore è sconosciuto, benché nota la distanza.
+Amore non è soggetto al Tempo, pur se rosee labbra e gote
+dovran cadere sotto la sua curva lama;
+Amore non muta in poche ore o settimane,
+ma impavido resiste al giorno estremo del giudizio:
+se questo è errore e mi sarà provato,
+io non ho mai scritto, e nessuno ha mai amato.";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bad words</title>
+</head>
+<body>
+    
+    <?php echo $sonetto; ?>
+</body>
+</html>
